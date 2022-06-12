5c5df89a7dd25c740f164644b5b2c4cce0bcd0a6
Roelfie <58694687+roelfie@users.noreply.github.com>
2022-06-12 23:41:51 +0200

    Add page header (to trigger a Travis build).

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
           <header>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
+          <h1 className="App-title">Fibonacci Calculator v2</h1>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
